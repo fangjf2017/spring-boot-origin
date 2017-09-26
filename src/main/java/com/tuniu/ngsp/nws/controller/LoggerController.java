494910abd653d49aac497e4670300b348c09a0d5
范帅 fanshuai (99999) <fanshuai@tuniu.com>
2017-09-26 15:18:31 +0800

    调整logback的日志配置

---
+++ src/main/java/com/tuniu/ngsp/nws/controller/LoggerController.java
@@ -0,0 +1,23 @@
+package com.tuniu.ngsp.nws.controller;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by fanshuai on 2017/9/26.
+ * 日志打印示例
+ */
+@RestController
+public class LoggerController {
+    private final static Logger logger = LoggerFactory.getLogger(LoggerController.class);
+
+    @RequestMapping("/log")
+    public String home() {
+        logger.info("logback 访问hello");
+        logger.error("logback 访问hello");
+        return "Hello World!";
+    }
+}
