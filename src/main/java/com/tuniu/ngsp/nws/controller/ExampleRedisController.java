159845d6db599c538c8c1eba59e443f663d8fefe
范帅 fanshuai (99999) <fanshuai@tuniu.com>
2017-09-30 10:32:50 +0800

    增加redis缓存

---
+++ src/main/java/com/tuniu/ngsp/nws/controller/ExampleRedisController.java
@@ -0,0 +1,31 @@
+package com.tuniu.ngsp.nws.controller;
+
+import com.tuniu.ngsp.nws.service.IRedisService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by fanshuai on 2017/9/30.
+ * redis使用demo
+ */
+@RestController
+public class ExampleRedisController {
+
+    @Autowired
+    private IRedisService redisService;
+
+
+    @RequestMapping("/redis/set")
+    public boolean redisSet(@RequestParam("value") String value) {
+        boolean isOk = redisService.set("car_name", value);
+        return isOk;
+    }
+
+    @RequestMapping("/redis/get")
+    public String redisGet() {
+        String name = redisService.get("car_name");
+        return name;
+    }
+}
