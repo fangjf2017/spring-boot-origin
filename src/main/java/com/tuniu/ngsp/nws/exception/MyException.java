600cdd6ad632acdbb4584ff5a1a9aa760dc81a93
范帅 fanshuai (99999) <fanshuai@tuniu.com>
2018-01-19 16:46:20 +0800

    增加全局的异常处理

---
+++ src/main/java/com/tuniu/ngsp/nws/exception/MyException.java
@@ -0,0 +1,16 @@
+package com.tuniu.ngsp.nws.exception;
+
+/**
+ * Created by fanshuai on 2018/1/19.
+ * 自定义的异常类型
+ */
+public class MyException extends Exception {
+
+    public MyException() {
+        super();
+    }
+
+    public MyException(String msg) {
+        super(msg);
+    }
+}
